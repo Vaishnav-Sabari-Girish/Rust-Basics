958d4e30ea4dd6535ee869bf1eb6e499c4009b4e
vaishnav_sabari_girish <forgamesonly684@gmail.com>
2025-02-02 11:25:17 +0530

    Variables (mutable and immutable

---
+++ variable/src/bin/immutable.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let variable: i32 = 42; //32 bit integer
+    println!("The given integer is : {}", variable);
+}
---
+++ variable/src/bin/mutable.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut variable = 42;
+    println!("Old variable value : {}", variable);
+
+    variable = variable + 1;
+
+    println!("New Variable Value : {}", variable);
+}
