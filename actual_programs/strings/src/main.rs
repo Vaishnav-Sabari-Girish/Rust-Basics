e9d45b194ca61226542418ca3cff724e92715b91
vaishnav_sabari_girish <forgamesonly684@gmail.com>
2025-04-11 19:37:50 +0530

    Added String slice programs

--- actual_programs/strings/src/main.rs
+++ actual_programs/strings/src/main.rs
@@ -4,4 +4,14 @@ fn main() {
     str_print.push_str(" Sabari Girish");    
     println!("New string: {}", str_print);
 
+    println!("");
+
+    println!("String Slices");
+    let word: String = String::from("Hello World");
+    let slice_full: &str = &word;
+    let slice_segmented: &str = &word[0..5];
+
+    println!("Sting slice (full) : {}", slice_full);
+    println!("String Slice (segmented): {}", slice_segmented);
+
 }
